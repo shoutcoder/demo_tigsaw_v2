67d078b2d99686e761dc103da6bc364d0e008ed6
shoutcoder <vishalvkv95@gmail.com>
2025-04-14 22:48:54 +0530

    first final draft

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Tigsaw Demo Store',
+  description: 'Tigsaw Demo Store'
 }
 
 export default function RootLayout({
