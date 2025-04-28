dc8a9d695791bc9135b3f441e9582ed564c7ffa1
Vishal Vishwakarma <48755143+shoutcoder@users.noreply.github.com>
2025-04-28 18:43:29 +0530

    Update skeleton-hero.tsx

--- components/skeleton-hero.tsx
+++ components/skeleton-hero.tsx
@@ -1,4 +1,4 @@
 export default function SkeletonHero() {
-  return <div className="w-full h-[300px] md:h-[400px] bg-slate-200 rounded-lg"></div>
+  return <div className="w-full h-[200px] md:h-[300px] bg-slate-200 rounded-lg"></div>
 }
 
